1af5101cb075187c45ab9a46334f6d81b08939cb
zhaowl <zwldh123@163.com>
2016-10-24 23:47:36 +0800

    Git托管
    将项目放到Git托管。

---
+++ app/src/main/java/com/example/zhao/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.zhao.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView text;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initWidget();
+    }
+
+    private void initWidget() {
+        text = (TextView) findViewById(R.id.text);
+        btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn:
+                text.setText("hahaha");
+                Intent intent = new Intent(this, ListViewActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+}
